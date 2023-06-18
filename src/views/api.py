387271f3560100e44f7a954cbfd25ed44442474c
Gustavo Voltolini <gustavoant.voltolini@gmail.com>
2023-06-18 08:37:15 -0300

    Creating FileId param

--- src/views/api.py
+++ src/views/api.py
@@ -1,10 +1,11 @@
 import json
+import uuid
 from uuid import uuid4
 from src.actions.tasks import task_process_shipment_file
 from src.actions.database_actions import DatabaseActions
 from fastapi import FastAPI, UploadFile, File, Body, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -38,11 +39,22 @@ async def request_token(payload=Body(...)):
 
 @app.post("/process_shipment_file")
 async def request_process_shipment_file(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
-    background_tasks.add_task(task_process_shipment_file, file=file)
-    return {"status": "Adicionada a fila de processamento!"}
+    file_id = generate_file_id()
+    background_tasks.add_task(task_process_shipment_file, file=file, file_id=file_id)
+    return {"status": "Adicionada a fila de processamento!", "file_id": file_id}
 
 
 @app.get("/historic")
 async def request_shipment_historic():
     database_actions = DatabaseActions()
     return database_actions.get_all_historic()
+
+
+@app.get("/download/<filename>")
+def download_file(filename: str):
+    file_path = "/send_shipment/" + filename
+    return FileResponse(file_path, filename="file.txt")
+
+
+def generate_file_id():
+    return uuid.uuid4()()
